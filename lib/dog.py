a3c0abef896162649a498af5787b7ff4f7ed0257
Duncan Kipkemoi <kipkemoiduncan7@gmail.com>
2023-05-19 00:54:06 +0300

    completed

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt", favourite_toy="Any"):
+        self.name = name
+        self.breed = breed
+        self.favorite_toy = favourite_toy
+
+    def bark(self):
+        print("Woof!")
+
+    def adopt(self, owner_name):
+        self.owner = owner_name
+
+
+fido = Dog('Fido')
+# print(fido.name)
+fido.owner = "Sophie"
+print(fido.owner)
+print(fido.breed)
+print(fido.favorite_toy)
+
+snoopy = Dog("Snoopy", "german", "Tennis Ball")
+print(snoopy.name)
+print(snoopy.breed)
+print(snoopy.favorite_toy)
+# print(snoopy.name)
